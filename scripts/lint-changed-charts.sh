17efcaba92e96fe090a9ff99dc856dab71509e8d
Brian Boerst <brian@boerst.com>
2023-09-10 13:27:04 -0400

    Adding basic linting

---
+++ scripts/lint-changed-charts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Get the list of changed files
+CHANGED_FILES=$(git diff HEAD~1 --name-only)
+
+# Loop through the list of changed files
+for FILE in $CHANGED_FILES; do
+  if [[ "$FILE" == *"Chart.yaml"* ]]; then
+    # Get the directory of the changed Chart.yaml
+    DIR=$(dirname "$FILE")
+    
+    # Navigate to the chart directory and run helm lint
+    helm lint "$DIR"
+  fi
+done
